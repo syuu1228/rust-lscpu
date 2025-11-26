644816ee1abbd4a9a6c6c49612de93c50150dcce
Takuya ASADA <syuu@dokukino.com>
2025-11-26 23:06:51 +0900

    implement hypervisor detection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 use std::fs;
 use std::collections::HashMap;
 use std::env;
-extern crate num_cpus;
+use std::arch::asm;
+use std::fmt;
 
 #[derive(Debug,PartialEq)]
 enum CpuByteOrder {
     LittleEndian,
     BigEndian,
+}
+
+#[derive(Clone,Copy)]
+enum HypervisorVendor {
+    KVM,
+    HyperV,
+    VMware,
+    Xen,
+    Parallels,
+    VirtualBox,
+    Unknown,
+}
+
+impl fmt::Display for HypervisorVendor {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HypervisorVendor::KVM => write!(f, "KVM"),
+            HypervisorVendor::HyperV => write!(f, "Hyper-V"),
+            HypervisorVendor::VMware => write!(f, "VMware"),
+            HypervisorVendor::Xen => write!(f, "Xen"),
+            HypervisorVendor::Parallels => write!(f, "Parallels"),
+            HypervisorVendor::VirtualBox => write!(f, "VirtualBox"),
+            HypervisorVendor::Unknown => write!(f, "Unknown"),
+        }
+    }
 }
 
 struct CpuInfo {
@@ -41,6 +67,9 @@ struct CpuInfo {
     cache_alignment: i32,
     address_sizes: String,
     power_management: Vec<String>,
+    ncpus: usize,
+    hypervisor_bit: bool,
+    hypervisor_vendor: Option<HypervisorVendor>,
 }
 
 impl CpuInfo {
@@ -80,6 +109,9 @@ impl CpuInfo {
             cache_alignment: cpuinfo[0].get("cache_alignment").unwrap().parse().unwrap(),
             address_sizes: cpuinfo[0].get("address sizes").unwrap().to_string(),
             power_management: CpuInfo::cpuinfo_parse_list(cpuinfo[0].get("power management").unwrap()),
+            ncpus: cpuinfo.len(),
+            hypervisor_bit: CpuInfo::cpuid_hypervisor_bit(),
+            hypervisor_vendor: CpuInfo::cpuid_hypervisor_vendor(),
         }
     }
 
@@ -168,6 +200,65 @@ impl CpuInfo {
         return cpuinfo;
     }
 
+    fn cpuid_hypervisor_bit() -> bool {
+        let eax: u32 = 0x01;
+        let ecx: u32;
+        unsafe {
+            asm!{
+                "cpuid",
+                in("eax") eax,
+                out("ecx") ecx
+            }
+        }
+        if ecx & (1 << 31) == (1 << 31) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    fn register_to_str(reg: u32) -> String {
+        let c0:char = (reg as u8) as char;
+        let c1:char = ((reg >> 8) as u8) as char;
+        let c2:char = ((reg >> 16) as u8) as char;
+        let c3:char = ((reg >> 24) as u8) as char;
+        return format!("{c0}{c1}{c2}{c3}");
+    }
+
+    fn cpuid_hypervisor_vendor() -> Option<HypervisorVendor> {
+        if !CpuInfo::cpuid_hypervisor_bit() {
+            return None;
+        }
+        let eax:u32 = 0x40000000;
+        let ebx:u32;
+        let ecx:u32;
+        let edx:u32;
+        unsafe {
+            asm!{
+                "cpuid",
+                "mov edi, ebx",
+                in("eax") eax,
+                out("edi") ebx,
+                out("ecx") ecx,
+                out("edx") edx
+            }
+        }
+        let str_ebx = CpuInfo::register_to_str(ebx);
+        let str_ecx = CpuInfo::register_to_str(ecx);
+        let str_edx = CpuInfo::register_to_str(edx);
+        let vendor_str = format!("{str_ebx}{str_ecx}{str_edx}");
+        let vendor = match *&(vendor_str.trim()) {
+            "KVMKVMKVM" => HypervisorVendor::KVM,
+            "Microsoft Hv" => HypervisorVendor::HyperV,
+            "VMwareVMware" => HypervisorVendor::VMware,
+            "XenVMMXenVMM" => HypervisorVendor::Xen,
+            "prl hyperv" => HypervisorVendor::Parallels,
+            "VboxVboxVbox" => HypervisorVendor::VirtualBox,
+            _ => HypervisorVendor::Unknown,
+        };
+        return Some(vendor);
+    }
+
     fn print_summary(&self) {
         println!("Architecture:\t\t\t{}", self.arch);
         let mut op_mode_string: &str;
@@ -178,7 +269,6 @@ impl CpuInfo {
         } else {
             op_mode_string = "64bit";
         }
-        {
         println!("CPU op-mode(s):\t\t\t{}", op_mode_string);
         println!("Address sizes:\t\t\t{}", self.address_sizes);
         if self.byteorder == CpuByteOrder::LittleEndian {
@@ -186,7 +276,7 @@ impl CpuInfo {
         } else {
             println!("Byte Order:\t\t\tBig Endian");
         }
-        println!("CPU(s):\t\t\t\t{}", num_cpus::get());
+        println!("CPU(s):\t\t\t\t{}", self.ncpus);
         println!("Vendor ID:\t\t\t{}", self.vendor_id);
         println!("  Model name:\t\t\t{}", self.model_name);
         println!("    CPU family:\t\t\t{}", self.cpu_family);
@@ -201,9 +291,11 @@ impl CpuInfo {
         if self.flags.iter().any(|f| f == "vmx") {
             println!("  Virtualization: VT-x");
         }
+        if let Some(result) = self.hypervisor_vendor {
+            println!("Hypervisor vendor:\t\t{}", result)
+        }
+        
     }
-    
-}
 }
 
 fn main() {
