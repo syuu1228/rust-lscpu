714d6d453ffe1f4977b13a04375a218271254c4e
Takuya ASADA <syuu@dokukino.com>
2025-12-02 20:16:47 +0900

    fix cpuid implementation

--- src/cpuid.rs
+++ src/cpuid.rs
@@ -4,52 +4,36 @@ pub struct Cpuid {
 }
 
 impl Cpuid {
-    const CPUID_FEAT_ECX_HYPERVISOR:u32 = 1 << 31;
-    const HYPERVISOR_INFO_LEAF:u32 = 0x40000000;
-
-    fn register_to_str(reg: u32) -> String {
-        let c0:char = (reg as u8) as char;
-        let c1:char = ((reg >> 8) as u8) as char;
-        let c2:char = ((reg >> 16) as u8) as char;
-        let c3:char = ((reg >> 24) as u8) as char;
-        return format!("{c0}{c1}{c2}{c3}");
-    }
-
     pub fn hypervisor_bit() -> bool {
-        let eax: u32 = 0x01;
+        let mut eax: u32 = 0x01;
         let ecx: u32;
         unsafe {
             asm!{
                 "cpuid",
-                in("eax") eax,
+                inout("eax") eax,
                 out("ecx") ecx
             }
         }
-        if ecx & Cpuid::CPUID_FEAT_ECX_HYPERVISOR == Cpuid::CPUID_FEAT_ECX_HYPERVISOR {
-            return true;
-        } else {
-            return false;
-        }
+        return (ecx & (1 << 31)) != 0;
     }
 
     pub fn hypervisor_vendor() -> String {
-        let eax:u32 = Cpuid::HYPERVISOR_INFO_LEAF;
-        let ebx:u32;
-        let ecx:u32;
-        let edx:u32;
+        let mut eax = 0x40000000;
+        let mut vendor_bytes = [0_u8; 4*3];
         unsafe {
             asm!{
+                "push rbx",
                 "cpuid",
-                "mov edi, ebx",
-                in("eax") eax,
-                out("edi") ebx,
-                out("ecx") ecx,
-                out("edx") edx
+                "mov [rdi], ebx",
+                "mov [rdi + 4], ecx",
+                "mov [rdi + 8], edx",
+                "pop rbx",
+                in("rdi") vendor_bytes.as_mut_ptr(),
+                inout("eax") eax,
+                out("ecx") _,
+                out("edx") _,
             }
         }
-        let str_ebx = Cpuid::register_to_str(ebx);
-        let str_ecx = Cpuid::register_to_str(ecx);
-        let str_edx = Cpuid::register_to_str(edx);
-        return format!("{str_ebx}{str_ecx}{str_edx}");
+        return String::from_utf8_lossy(&vendor_bytes).to_string();
     }
 }
